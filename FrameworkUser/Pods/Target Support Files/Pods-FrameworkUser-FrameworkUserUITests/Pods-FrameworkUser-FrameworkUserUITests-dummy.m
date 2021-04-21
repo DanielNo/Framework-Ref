58843e0577572aef0ac9f2c361de74c034b89f89
Daniel No <>
2021-04-21 13:37:58 -0400

    create project and init cocoapods

---
+++ FrameworkUser/Pods/Target Support Files/Pods-FrameworkUser-FrameworkUserUITests/Pods-FrameworkUser-FrameworkUserUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrameworkUser_FrameworkUserUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FrameworkUser_FrameworkUserUITests
+@end
